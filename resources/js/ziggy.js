2b5ed8b91b2468bb3662b2a4a9ea338fa7ecc5a6
nblkmal <nabil@runcloud.io>
2022-09-13 17:41:29 +0800

    complete installation laravel-vue3-inertia

---
+++ resources/js/ziggy.js
@@ -0,0 +1,7 @@
+const Ziggy = {"url":"http:\/\/runcloud-scrutiny.test","port":null,"defaults":{},"routes":[]};
+
+if (typeof window !== 'undefined' && typeof window.Ziggy !== 'undefined') {
+    Object.assign(Ziggy.routes, window.Ziggy.routes);
+}
+
+export { Ziggy };
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get(
+    '/',
+    function () {
+        return Inertia::render(
+            'Home',
+            [
+                'title' => 'Homepage',
+            ]
+        );
+    }
+)->name( 'homepage' );
+
+Route::get(
+    '/about',
+    function () {
+        return Inertia::render(
+            'About',
+            [
+                'title' => 'About',
+            ]
+        );
+    }
+)->name( 'about' );
