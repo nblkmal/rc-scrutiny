937cfc8cbe768fad516e6fcf28ad1e7e8b6d9245
nblkmal <nabil@runcloud.io>
2022-09-14 17:43:06 +0800

    chore : wip add solar-ui

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,6 +6,16 @@ mix.alias({
     ziggy: path.resolve("vendor/tightenco/ziggy/dist/vue"),
 });
 
+mix.styles([
+    'node_modules/solar-ui/packages/core/lib/style.css',
+    'node_modules/solar-ui/packages/server/lib/style.css',
+    'node_modules/solar-ui/packages/webapp/lib/style.css',
+    'node_modules/solar-ui/packages/settings/lib/style.css'
+], 'public/assets/css/app.css')
+
+/** Include img assets from `solar-ui/core` */
+mix.copyDirectory('node_modules/solar-ui/packages/core/src/img', path.join(__dirname, 'public/assets/img')) 
+
 // Build files
 mix.js("resources/js/app.js", "public/js")
     .vue({ version: 3 })
